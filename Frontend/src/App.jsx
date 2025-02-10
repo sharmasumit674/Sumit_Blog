1a59e372cc2d0f4df43e68ad15ea9d41cfd4005a
Sumit <Sumit03987@gmail.com>
2025-02-11 00:58:19 +0530

    Create pages and routes

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,11 +1,25 @@
-import React from 'react'
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./Pages/Home";
+import Signup from "./Pages/Signup";
+import Signin from "./Pages/Signin";
+import Dashboard from "./Pages/Dashboard";
+import Projects from "./Pages/Projects";
+import About from "./pages/About";
 
 const App = () => {
   return (
-    <div>
-     <h1 className='text-3xl text-red-400'>Hello</h1>
-    </div>
-  )
-}
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/signin" element={<Signin />} />
+        <Route path="/dashboard" element={<Dashboard/>}/>
+        <Route path="/projects" element={<Projects/>}/>
+      </Routes>
+    </BrowserRouter>
+  );
+};
 
-export default App
+export default App;
---
+++ Frontend/src/Pages/Signin.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Signin = () => {
+  return (
+    <div>
+      singin
+    </div>
+  )
+}
+
+export default Signin

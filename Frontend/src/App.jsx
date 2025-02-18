a01bfa3956e416f89592272557775b76d8a563f0
Sumit <Sumit03987@gmail.com>
2025-02-18 21:11:48 +0530

    create the header section

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -6,10 +6,12 @@ import Signin from "./Pages/Signin";
 import Dashboard from "./Pages/Dashboard";
 import Projects from "./Pages/Projects";
 import About from "./pages/About";
+import Header from "./Components/Header";
 
 const App = () => {
   return (
     <BrowserRouter>
+    <Header/>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
